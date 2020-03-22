a951209ed07df851097dbf546185724a71930d91
Thomas Roberts <robertsorange@gmail.com>
2020-03-22 17:33:38 -0600

    Combined state scripts into 1

---
+++ AgricultureManager/Assets/Scripts/StateController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StateController : MonoBehaviour
+{
+    public string stateName;
+    public Text stateText;
+    public Image image;
+    public Sprite sprite;
+
+    private void OnMouseDown() {
+        stateText.text = $"State: {stateName}";
+        image.sprite = sprite;
+    }
+}
