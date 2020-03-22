10f9878686309972dc6c5d44ac915dfee78c765b
Thomas Roberts <robertsorange@gmail.com>
2020-03-22 17:09:17 -0600

    Reset files

---
+++ AgricultureManager/Assets/Scripts/LightBlueScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightBlueScript : MonoBehaviour
+{
+    [SerializeField]
+    public Text stateText = null;
+    public Image image = null;
+    public Sprite sprite = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        stateText.text = "State: LightBlue";
+        image.sprite = sprite;
+    }
+}
