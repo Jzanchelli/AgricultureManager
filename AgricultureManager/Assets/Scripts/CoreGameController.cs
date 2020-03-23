e8d7ce8dbdddde7a7b356ed5c24c764785ee668a
Thomas Roberts <robertsorange@gmail.com>
2020-03-22 22:40:37 -0600

    Changing state values when buy button pressed

--- AgricultureManager/Assets/Scripts/CoreGameController.cs
+++ AgricultureManager/Assets/Scripts/CoreGameController.cs
@@ -7,7 +7,15 @@ using UnityEngine.UI;
 public class CoreGameController : MonoBehaviour
 {
     public Text yearText;
-    public string activeState;    
+    // The active state is updated in our "OnClick" event, it should not be manually set in the inspector
+    [HideInInspector]
+    public string activeState;
+
+    public Slider cowSlider;
+    public Slider grainSlider;
+
+    public int cowCost = 10;
+    public int grainCost = 10;
 
     void Start()
     {
@@ -17,6 +25,24 @@ public class CoreGameController : MonoBehaviour
     public void UpdateState() {
         // Any changes made to this state will then affect the states below it
         State toUpdate = LookupState();
+
+        int cowsToBuy = (int)cowSlider.value;
+        int grainToBuy = (int)grainSlider.value;
+
+        // Check if able to buy this amount
+        int cowDollars = cowsToBuy * cowCost;
+        int grainDollars = grainToBuy * grainCost;
+        
+        if(cowDollars + grainDollars > toUpdate.dollars) {
+            // Unable to purchase. Alert the user in some way
+            Debug.LogWarning("Unable to purchase this amount of grains/cows");
+        }
+        else {
+            Debug.Log("Purchase successful");
+            toUpdate.numCows += cowsToBuy;
+            toUpdate.numGrains += grainToBuy;
+            toUpdate.dollars -= (cowDollars + grainDollars);
+        }
 
     }
 
