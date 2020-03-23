3841c9e35acfe07110c455a76d8f08d0399c1b76
Thomas Roberts <robertsorange@gmail.com>
2020-03-22 22:32:16 -0600

    Updating slider value when dragged

---
+++ AgricultureManager/Assets/DisplaySliderValue.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplaySliderValue : MonoBehaviour
+{
+    private Text text;
+    public Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();    
+    }
+    // The slider is supposed to work like this: https://www.youtube.com/watch?v=b3S5a_ohZZ0
+    // I couldn't get it, so I have to do it the hard way
+    // This gets called by the slider when it changes value (see it's events)
+    public void UpdateTextValue() {
+        text.text = slider.value.ToString();
+    }
+}
