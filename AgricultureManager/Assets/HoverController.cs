f40ba26221f32177c1e46f503391c4f7306683bd
Thomas Roberts <robertsorange@gmail.com>
2020-04-09 14:29:31 -0600

    Fixed scale issue on text

--- AgricultureManager/Assets/HoverController.cs
+++ AgricultureManager/Assets/HoverController.cs
@@ -6,7 +6,6 @@ public class HoverController : MonoBehaviour
     private CanvasGroup canvasGroup;
     public Vector3 offset;
     public new Camera camera;
-    public RectTransform BasisObject;
 
     //Text boxes to show
     public Text moneyText;
@@ -15,14 +14,18 @@ public class HoverController : MonoBehaviour
     public Text grainText;
 
     public float fadeTime = 0.5f;
+    private float initialZ;
 
     void Start() {
         canvasGroup = GetComponent<CanvasGroup>();
+
+        initialZ = transform.position.z;
     }
 
     private void Update() {
         Vector3 mousePosition = Input.mousePosition + offset;
-        mousePosition.z = BasisObject.position.z;
+        mousePosition.z = initialZ;
+        // Need to convert from the screen position to our world's position
         transform.position = camera.ScreenToWorldPoint(mousePosition);
     }
 
@@ -36,12 +39,13 @@ public class HoverController : MonoBehaviour
 
     public void Show(State state) {
         LeanTween.cancel(gameObject);
-        LeanTween.value(gameObject, TweenCallback, canvasGroup.alpha, 1f, fadeTime);
 
         moneyText.text = $"Money: ${state.dollars}";
         co2Text.text = $"Co2: {state.co2Emissions}";
         cowText.text = $"Cows: {state.numCows}";
         grainText.text = $"Grains: {state.numGrains}";
+
+        LeanTween.value(gameObject, TweenCallback, canvasGroup.alpha, 1f, fadeTime);
     }
 
     void TweenCallback(float value) {
