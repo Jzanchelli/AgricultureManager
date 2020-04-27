6e2b75f8958ac250c6fc10c3e9a68b3c8260f512
Thomas Roberts <robertsorange@gmail.com>
2020-04-27 11:41:30 -0600

    Consolidated fader scripts

---
+++ AgricultureManager/Assets/Scripts/Fader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fader : MonoBehaviour
+{
+    [Range(0, 2)]
+    public float fadeTime = 0.5f;
+    public bool increaseAlpha = true;
+
+    private CanvasGroup canvas;
+    void Start() {
+        float startValue = increaseAlpha ? 0 : 1;
+        float endValue = increaseAlpha ? 1 : 0;
+        canvas = GetComponent<CanvasGroup>();
+
+        canvas.alpha = startValue;
+
+        LeanTween.value(gameObject,
+            (val) => canvas.alpha = val,
+            startValue, endValue, fadeTime);
+    }
+}
